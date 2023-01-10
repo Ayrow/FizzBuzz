e094dfa04a06eb5f5f3d44071ccedf5a039c33ff
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-10 14:04:54 +0100

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Aymeric Pilaert on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
