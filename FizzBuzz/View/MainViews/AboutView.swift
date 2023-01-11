80d69a87ea16767dc392b0bc35c0bcf68aecd98f
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-11 11:32:10 +0100

    Completed the about view adding background image view

--- FizzBuzz/View/MainViews/AboutView.swift
+++ FizzBuzz/View/MainViews/AboutView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         ZStack {
-            Color.blue.opacity(0.5).ignoresSafeArea()
+            BackgroundImageView()
+                .opacity(0.3)
             
             Text("""
 Fizz buzz is a group word game for children to teach them about division.[1] Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz".
@@ -15,6 +16,8 @@ Fizz buzz (often spelled FizzBuzz in this context) has been used as an interview
             .fontWeight(.semibold)
             .multilineTextAlignment(.leading)
             .minimumScaleFactor(0.1)
+            .padding()
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
             .padding()
         }
     }
