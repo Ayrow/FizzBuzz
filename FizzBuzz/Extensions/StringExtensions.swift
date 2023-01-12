196ed13e40b4336952e4294bddce53e2bbb5ae9b
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-12 10:05:39 +0100

    setup indent string extension

---
+++ FizzBuzz/Extensions/StringExtensions.swift
@@ -0,0 +1,16 @@
+
+
+import Foundation
+
+extension String {
+    
+    func indent() -> String {
+        var result = ""
+        let listOfLines = self.split(separator: "\n")
+        listOfLines.forEach {
+            line in
+            result += "\t\(line)\n"
+        }
+        return String(result.dropLast())
+    }
+}
