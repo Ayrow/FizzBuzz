5fdd13f06167c6df00a5d383231a60cf4bb5ed49
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-12 10:24:42 +0100

    Completed the FizzBuzz model

--- FizzBuzz/Model/FizzBuzzAlgModel.swift
+++ FizzBuzz/Model/FizzBuzzAlgModel.swift
@@ -27,18 +27,53 @@ struct FizzBuzzAlgModel: Identifiable {
     var boilerWrapper: (String) -> String = { code in
         return code
     }
-    let maxConstDefinition: String
+    let maxConstDef: String
     let loopWrapper: (String) -> String
-    let print: (String) -> String
+    let printString: (String) -> String
     let printInt: () -> String
     var equality: String = "=="
     var moduloSymbol: String = "%"
     let ifStatement: (String, String) -> String
-    let ifElseStatement: (String, String) -> String
+    let elseifStatement: (String, String) -> String
     let elseStatement: (String) -> String
     
     var actualFizzBuzzCode : String {
-        "FizzBuzz in \(name)"
+        let mod3 = "i \(moduloSymbol) 3 \(equality) 0"
+        let codeFizz = printString("Fizz")
+        
+        let mod5 = "i \(moduloSymbol) 5 \(equality) 0"
+        let codeBuzz = printString("Buzz")
+        
+        let mod15 = "i \(moduloSymbol) 15 \(equality) 0"
+        let codeFizzBuzz = printString("FizzBuzz")
+        
+        let printInt = printInt()
+        
+        let codeInLoop = """
+\(ifStatement(mod15, codeFizzBuzz))
+    \(elseifStatement(mod3, codeFizz.indent()))
+    \(elseifStatement(mod5, codeBuzz.indent()))
+    \(elseStatement(printInt.indent()))
+"""
+        
+        let mainCode = """
+\(maxConstDef)
+
+\(loopWrapper(codeInLoop))
+
+"""
+        var versionString = ""
+        
+        if let versionNumber = versionNumber {
+            versionString = " implementation Version \(versionNumber)"
+        }
+        
+        return """
+\(commentCode("FizzBuzz \(versionString) is implemented in the \(name) programming language."))
+\(imports)
+\(boilerWrapper(mainCode))
+
+"""
+        
     }
-    
 }
--- FizzBuzz/ModelView/FizzBuzzAlgViewModel.swift
+++ FizzBuzz/ModelView/FizzBuzzAlgViewModel.swift
@@ -8,14 +8,14 @@ class FizzBuzzAlgViewModel: ObservableObject {
         fizzBuzzModels = [
             .init(
                 name: "Swift",
-                maxConstDefinition: "let MAX = 100",
+                maxConstDef: "let MAX = 100",
                 loopWrapper: {code in
                 """
                 for i in 1...MAX {
                 \(code)}
                 """
                 },
-                print: {str in
+                printString: {str in
                     #"print("\#(str)\t", terminator: "")"#
                 },
                 printInt: {
@@ -28,7 +28,7 @@ class FizzBuzzAlgViewModel: ObservableObject {
                     }
                     """
                 },
-                ifElseStatement: {logic, code in
+                elseifStatement: {logic, code in
                     """
                     else if \(logic) {
                     \(code)
@@ -55,14 +55,14 @@ class FizzBuzzAlgViewModel: ObservableObject {
                 }
                 """
                 },
-                maxConstDefinition: "#define MAX 100",
+                maxConstDef: "#define MAX 100",
                 loopWrapper: {code in
                 """
                 for(int i = 1; i <= MAX; i++) {
                 \(code)}
                 """
                 },
-                print: {str in
+                printString: {str in
                     #"printf("\#(str)\t");"#
                 },
                 printInt: {
@@ -75,7 +75,7 @@ class FizzBuzzAlgViewModel: ObservableObject {
                     }
                     """
                 },
-                ifElseStatement: {logic, code in
+                elseifStatement: {logic, code in
                     """
                     else if (\(logic)) {
                     \(code)
@@ -89,7 +89,181 @@ class FizzBuzzAlgViewModel: ObservableObject {
                         \(code)
                         }
                         """
-                })
+                }),
+            .init(
+                name: "Python",
+                commentCode: { code in return #"# "# + code },
+                maxConstDef: "MAX = 100",
+                loopWrapper: { code in
+                """
+                for i in range(1,MAX):
+                    \(code)
+                """
+                },
+                printString: { str in
+                    #"print("\#(str)\t", end = "")"#
+                },
+                printInt: {#"print(f"{i}\t", end = "")"#},
+                ifStatement: { logic, code in
+                    """
+                    if \(logic):
+                        \(code.indent())
+                    """
+                },
+                elseifStatement: { logic, code in
+                    """
+                    
+                        elif \(logic):
+                        \(code)
+                    """
+                }, elseStatement: { code in
+                    """
+                    
+                        else:
+                        \(code)
+                    """
+                }
+            ),
+            .init(
+                name: "Golang",
+                commentCode: { code in return #"// "# + code },
+                versionNumber: 1.0,
+                imports: "package main\n\nimport \"fmt\"\n",
+                boilerWrapper: { code in
+                """
+                func main() {
+                \(code.indent())
+                }
+                """
+                },
+                maxConstDef: "const MAX = 100",
+                loopWrapper: { code in
+                """
+                for i := 1; i < MAX; i++ {
+                    \(code)
+                }
+                """
+                },
+                printString: { str in
+                    #"fmt.Print("\#(str)\t")"#
+                },
+                printInt: {#"fmt.Printf("%d\t", i)"#},
+                ifStatement: { logic, code in
+                    """
+                    if (\(logic)) {
+                        \(code.indent())
+                        }
+                    """
+                },
+                elseifStatement: { logic, code in
+                    """
+                    else if (\(logic)) {
+                        \(code)
+                        }
+                    """
+                }, elseStatement: { code in
+                    """
+                    else {
+                        \(code)
+                        }
+                    """
+                }
+            ),
+            .init(
+                name: "Rust",
+                commentCode: { code in return #"// "# + code },
+                versionNumber: 1.0,
+                imports: "",
+                boilerWrapper: { code in
+                """
+                fn main() {
+                \(code.indent())
+                }
+                """
+                },
+                maxConstDef: "const MAX: i32 = 100;",
+                loopWrapper: { code in
+                """
+                for i in 1..MAX+1 {
+                    \(code)
+                }
+                """
+                },
+                printString: { str in
+                    #"print!("\#(str)\t");"#
+                },
+                printInt: {#"print!("{}\t", i);"#},
+                ifStatement: { logic, code in
+                    """
+                    if \(logic) {
+                        \(code.indent())
+                        }
+                    """
+                },
+                elseifStatement: { logic, code in
+                    """
+                    else if \(logic) {
+                        \(code)
+                        }
+                    """
+                }, elseStatement: { code in
+                    """
+                    else {
+                        \(code)
+                        }
+                    """
+                }
+            ),
+            .init(
+                name: "HTML/JavaScript",
+                commentCode: { code in return "<!-- \(code) -->" },
+                versionNumber: 0.7,
+                imports: "",
+                boilerWrapper: { code in
+                """
+                <h1>Implementation of FuzzBuzz in HTML/Javascript</h1>
+                <pre><div id="result">
+                
+                </div></pre>
+                <script>
+                document.getElementById('result');
+                \(code.indent())
+                </script>
+                """
+                },
+                maxConstDef: "var MAX = 100",
+                loopWrapper: { code in
+                """
+                for( var i = 1; i <= MAX; i++) {
+                    \(code)
+                }
+                """
+                },
+                printString: { str in
+                    #"result.innerHTML=result.innerHTML+"\#(str)\t";"#
+                },
+                printInt: {#"result.innerHTML=result.innerHTML+i+"\t""#},
+                ifStatement: { logic, code in
+                    """
+                    if (\(logic)) {
+                        \(code.indent())
+                        }
+                    """
+                },
+                elseifStatement: { logic, code in
+                    """
+                    else if (\(logic)) {
+                        \(code)
+                        }
+                    """
+                }, elseStatement: { code in
+                    """
+                    else {
+                        \(code)
+                        }
+                    """
+                }
+            ),
         
         ]
     }
